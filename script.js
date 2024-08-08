1a35c6a89421dae6b5d6ad510ea275dc40216bcb
ria-helluva-boss <alexabliss1003@yandex.by>
2024-08-08 20:51:56 +0300

    change data.json , add JS and HTML code

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-function fetchMovies(){
+document.addEventListener("DOMContentLoaded", function () {
+  fetchMovies();
+});
+
+function fetchMovies() {
   fetch("data.json")
   .then((response) => response.json())
   .then((jsonData) => {
-    
-  })
+  displayMovies(jsonData, "Комедия", ".comedy-gallery .swiper-wrapper");
+  displayMovies(jsonData, "Ужасы", ".horror-gallery .swiper-wrapper");
+  });
+}
+
+function displayMovies(movies, genre, wrapperSelector) {
+  const swiperWrapper = document.querySelector(wrapperSelector);
+
+  for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+
+      if (movie.genre === genre){
+          const swiperSlide = document.createElement('div');
+          swiperSlide.classList.add('swiper-slide');
+
+          const imageSlider = document.createElement('div');
+          imageSlider.classList.add('image-slider');
+
+          const image = document.createElement('img');
+          image.src = movie.url;
+          image.alt = movie.name;
+
+          const movieName = document.createElement('h3');
+          movieName.classList.add('movie-name');
+          movieName.textContent = movie.name;
+
+          imageSlider.appendChild(image);
+          imageSlider.appendChild(movieName);
+          swiperSlide.appendChild(imageSlider);
+          swiperWrapper.appendChild(swiperSlide);
+      }
+  }
 }

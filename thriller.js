65c1b861b04edfdb29faf254118f54b56dd74f87
elencodes <playnonstop205@gmail.com>
2024-08-14 02:34:32 +0400

    fixed styles for 2 pages, fixed js for 2 pages and added validation for username input (reviews films)

--- thriller.js
+++ thriller.js
@@ -58,4 +58,102 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 	showThrillerFilm()
+
+	//Отзывы
+
+	function loadReviews() {
+		const reviewsList = document.getElementById('reviewsList');
+		const reviewsThriller = JSON.parse(localStorage.getItem('reviewsThriller')) || []; //получаем и парсим ниже подробнее будет
+		reviewsList.innerHTML = reviewsThriller.map(review => `  
+			  <div class="review-item">
+					<h3>${review.reviewName} (${review.reviewRating})</h3>
+					<p>${review.reviewComment}</p>
+			  </div>
+		 `).join(''); //проходимся по каждому мапом создаём новые массивы и создаём разметку отзыва
+	}
+
+	function saveReview(review) { //парсим и ищем по ключу 'reviews'  если ничего нет создаём пустой массив если есть добавляем отзыв и отправляем обратно
+		const reviewsThriller = JSON.parse(localStorage.getItem('reviewsThriller')) || [];
+		reviewsThriller.push(review);
+		localStorage.setItem('reviewsThriller', JSON.stringify(reviewsThriller));
+	}
+
+	document.getElementById('reviewForm').addEventListener('submit', (event) => {
+		event.preventDefault(); //сбрасываем естественное сосотояние и получаем значения из формы при отправке формы при нажатии submit кнопки
+		checkFormValidityName();
+		const reviewName = document.getElementById('reviewName').value;
+		const reviewComment = document.getElementById('reviewComment').value;
+		const reviewRating = document.getElementById('reviewRating').value;
+
+		if (reviewName && reviewComment && reviewRating) {
+			const review = {
+				reviewName,
+				reviewComment,
+				reviewRating
+			};
+			displayReview(review); // тут отзыв отображается на странице через другую функцию ниже
+			saveReview(review); // тут мы через функцию выше в локал сторадж сохраняем
+			document.getElementById('reviewForm').reset(); //тут очистка формы после отправки методом .reset()
+		}
+	});
+
+	function displayReview(review) {
+		const reviewsList = document.getElementById('reviewsList');
+		const reviewElement = document.createElement('div');
+		reviewElement.className = 'review-item';
+		reviewElement.innerHTML = `
+			  <h3>${review.reviewName} (${review.reviewRating})</h3>
+			  <p>${review.reviewComment}</p>
+		 `;
+		reviewsList.appendChild(reviewElement);
+	}
+	loadReviews();
+
+	//Добавляем заголовок в блок отзывы
+	const reviewsList = document.getElementById('reviewsList');
+	reviewsList.innerHTML = `<h2>Ваши отзывы</h2>`
+
+	//Валидация имени - проверка имени на корректность введенных данных
+	function validateName(name) {
+		let regex = /^[A-Za-z\s]+$/g; //содержит только буквы и пробелы
+		return regex.test(name);
+	}
+
+	//Ищем объекты формы в HTML документе для вывода сообщения об ошибке
+	const errorUserName = document.querySelector(`.error__userName`);
+
+	//Ищем объекты формы (имя) в HTML документе
+	const reviewName = document.getElementById('reviewName');
+
+	//Расширенная валидация имени пользователя (функция еще не вызывается)
+	function checkFormValidityName() {
+		if (reviewName.value === "") {
+			errorUserName.textContent = `Введите Ваше имя`;
+			errorUserName.classList.add('error__message');
+			reviewName.classList.add('error');
+			reviewName.style.margin = "0 0 1rem 0";
+		} else if (reviewName.value.length < 2) {
+			//(если имя введено некорректно - появляются сообщения об ошибке)
+			errorUserName.textContent = `Имя должно содержать от 2 символов`;
+			errorUserName.classList.add('error__message');
+			reviewName.classList.add('error');
+			reviewName.style.margin = "0 0 1rem 0";
+		} else if (validateName(reviewName.value) === false) {
+			//(если имя введено некорректно - появляются сообщения об ошибке)
+			errorUserName.textContent = `Имя должно содержать латинские буквы и пробелы`;
+			errorUserName.classList.add('error__message');
+			reviewName.classList.add('error');
+			reviewName.style.margin = "0 0 1rem 0";
+		} else {
+			//(если имя введено корректно - сообщения об ошибке исчезают)
+			errorUserName.textContent = ``;
+			errorUserName.classList.remove('error__message');
+			reviewName.classList.remove('error');
+			reviewName.style.margin = "0 0 1rem 0";
+		}
+	}
+
+	//Проверка на валидность введенного имени
+	//(событие input срабатывает при вводе или удалении каждого символа)
+	reviewName.addEventListener(`change`, checkFormValidityName);
 })

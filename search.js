1867e446ee7f59807b6ea583055df1b9d51c2e02
elencodes <playnonstop205@gmail.com>
2024-08-16 23:11:58 +0400

    fixed adaptive for navigation menu and search page

--- search.js
+++ search.js
@@ -7,7 +7,7 @@ async function loadJSON() {
 
 // Функция для поиска
 function search(query) {
-	const results = jsonData.filter(item => 
+	const results = jsonData.filter(item =>
 		item.name.toLowerCase().includes(query.toLowerCase()) || item.director.toLowerCase().includes(query.toLowerCase()) || item.genre.toLowerCase().includes(query.toLowerCase()));
 	displayResults(results);
 }
@@ -15,7 +15,7 @@ function search(query) {
 // Вывод результатов на экран
 function displayResults(results) {
 	const resultsDiv = document.getElementById('results--search');
-	resultsDiv.innerHTML = ''; 
+	resultsDiv.innerHTML = '';
 
 	if (results.length === 0) {
 		resultsDiv.innerHTML = '<p>Фильм отсутствует в нашей библиотеке</p>';
@@ -24,48 +24,49 @@ function displayResults(results) {
 
 	results.forEach(item => {
 		const nameFilm = document.createElement('h2');
-		nameFilm.textContent = item.name; 
+		nameFilm.textContent = item.name;
 		resultsDiv.appendChild(nameFilm);
 		//добавляем ссылку на страницу дом дракона
-		if(item.name=="Дом Дракона"){
+		if (item.name == "Дом Дракона") {
 			const imageUrl = item.url;
 			const linkUrl = "dom-drakona.html";
 			const link = document.createElement('a');
 			link.href = linkUrl;
-			const filmPoster  = document.createElement('img');
+			const filmPoster = document.createElement('img');
 			filmPoster.className = "img--serach";
 			filmPoster.src = imageUrl;
-			link.appendChild(filmPoster );
+			link.appendChild(filmPoster);
 			resultsDiv.appendChild(link);
-		}else if(item.name=="Исчезнувшая"){
+		} else if (item.name == "Исчезнувшая") {
 			const imageUrl = item.url;
 			const linkUrl = "gone-girl.html";
 			const link = document.createElement('a');
 			link.href = linkUrl;
-			const filmPoster  = document.createElement('img');
+			const filmPoster = document.createElement('img');
 			filmPoster.className = "img--serach";
 			filmPoster.src = imageUrl;
-			link.appendChild(filmPoster );
+			link.appendChild(filmPoster);
 			resultsDiv.appendChild(link);
-		} else{		
-		const filmPoster = document.createElement('img');
-		filmPoster.src =  item.url; 
-		filmPoster.className = "img--serach";
-		resultsDiv.appendChild(filmPoster);};
+		} else {
+			const filmPoster = document.createElement('img');
+			filmPoster.src = item.url;
+			filmPoster.className = "img--serach";
+			resultsDiv.appendChild(filmPoster);
+		};
 		const rating = document.createElement('h3');
-		rating.textContent = "Рэйтинг: "+item.rating; 
+		rating.textContent = "Рейтинг: " + item.rating;
 		resultsDiv.appendChild(rating);
 		const year = document.createElement('h3');
-		year.textContent = "Год: "+item.year; 
+		year.textContent = "Год: " + item.year;
 		resultsDiv.appendChild(year);
 		const filmGenre = document.createElement('h3');
-		filmGenre.textContent = "Жанр: "+ item.genre; 
+		filmGenre.textContent = "Жанр: " + item.genre;
 		resultsDiv.appendChild(filmGenre);
 		const filmDirector = document.createElement('h3');
-		filmDirector.textContent = "Режисер: "+ item.director; 
+		filmDirector.textContent = "Режисер: " + item.director;
 		resultsDiv.appendChild(filmDirector);
 		const filmInfo = document.createElement('p');
-		filmInfo.textContent =  item.info; 
+		filmInfo.textContent = item.info;
 		resultsDiv.appendChild(filmInfo);
 
 	});
@@ -74,9 +75,9 @@ function displayResults(results) {
 // кнопка поиска
 document.getElementById('searchButton').addEventListener('click', () => {
 	const query = document.getElementById('search').value;
-	if(query!=""){
-	search(query)}
-	else{
+	if (query != "") {
+		search(query)
+	} else {
 		let res = document.getElementById('results');
 		res.innerHTML = "Введите данные для поиска";
 	};
@@ -85,15 +86,15 @@ document.getElementById('searchButton').addEventListener('click', () => {
 // поиск через нажатие клавиши Enter
 document.getElementById('search').addEventListener('keydown', (event) => {
 	if (event.key === 'Enter') {
-	  const query = document.getElementById('search').value;
-	  if (query != "") {
-		search(query);
-	  } else {
-		let res = document.getElementById('results');
-		res.innerHTML = "Введите данные для поиска";
-	  }
-	  document.getElementById('search').value = '';
+		const query = document.getElementById('search').value;
+		if (query != "") {
+			search(query);
+		} else {
+			let res = document.getElementById('results');
+			res.innerHTML = "Введите данные для поиска";
+		}
+		document.getElementById('search').value = '';
 	}
-  });
+});
 
 loadJSON();
